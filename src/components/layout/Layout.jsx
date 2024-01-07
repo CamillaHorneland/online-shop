47dbec7a9cdc54da0516a8d764add3cf98386897
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2024-01-07 04:00:33 +0100

    footer

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./nav/Nav";
+import Footer from "./footer/Footer";
 
 
 export default function Layout() {
@@ -7,6 +8,7 @@ export default function Layout() {
 		<div>
 			<Nav />
 			<Outlet />
+			<Footer />
 		</div>
 	);
 }
