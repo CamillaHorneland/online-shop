65a5468a09866e55c282f127cc9772beb33f33b4
Camilla <104870685+CamillaHorneland@users.noreply.github.com>
2024-01-09 05:48:18 +0100

    cart working, counting working ok, checkoutsuccess working

--- src/hooks/CartProvider.jsx
+++ src/hooks/CartProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect} from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { getStorageValue, useLocalStorage } from './LocaleStorage';
 
 export const CartContext = createContext({
@@ -14,40 +14,44 @@ export const _addToCart = (product) => {
   var tmpCartProductList = tmpCart.products.filter(cartProduct => cartProduct.id != product.id);
   var cartListProductItem = { ...product, qty: existing ? existing.qty + 1 : 1 };
   tmpCartProductList.push(cartListProductItem);
-  const newCart = {products: tmpCartProductList};
+  const newCart = { products: tmpCartProductList };
   localStorage.setItem('cart', JSON.stringify(newCart));
   return newCart;
-}
+};
 
 export const _removeFromCart = (product) => {
   var tmpCart = getStorageValue('cart');
   var existing = tmpCart.products.find(cartProduct => cartProduct.id == product.id);
   var tmpCartProductList = tmpCart.products.filter(cartProduct => cartProduct.id != product.id);
-  const newCart = {products: tmpCartProductList}
-
-  if(existing && existing.qty > 1) {
+  const newCart = { products: tmpCartProductList };
+  
+  if (existing && existing.qty > 1) {
     existing.qty = existing.qty - 1;
     tmpCartProductList.push(existing);
   }
-console.log(newCart);
+  console.log(newCart);
   localStorage.setItem('cart', JSON.stringify(newCart));
   return newCart;
-}
+};
 
-
+export const _clearCart = () => {
+  const newCart = { products: [] };
+  localStorage.setItem('cart', JSON.stringify(newCart));
+  return newCart;
+};
 
 export default function CartProvider({ children }) {
   const initialCart = getStorageValue('cart', { products: [] });
   const [cart, setCart] = useLocalStorage('cart', initialCart);
   const [quantity, setQuantity] = useState(cart.products.length || 0);
 
-  const addToCart = (product) => {
-    const updatedCart = _addToCart(product);
-    setCart(updatedCart);
+   const clearCart = () => {
+    _clearCart();
+    setQuantity(0); 
   };
 
-  const removeFromCart = (product) => {
-    const updatedCart = _removeFromCart(product);
+  const addToCart = (product) => {
+    const updatedCart = _addToCart(product);
     setCart(updatedCart);
   };
 
@@ -55,12 +59,17 @@ export default function CartProvider({ children }) {
     return cart.products;
   };
 
+  const removeFromCart = (product) => {
+    const updatedCart = _removeFromCart(product);
+    setCart(updatedCart);
+  };
+
   useEffect(() => {
     setQuantity(cart.products.length);
   }, [cart.products]);
 
   return (
-    <CartContext.Provider value={{ quantity, addToCart, removeFromCart, getCartProducts }}>
+    <CartContext.Provider value={{ quantity, addToCart, removeFromCart, getCartProducts, clearCart }}>
       {children}
     </CartContext.Provider>
   );
